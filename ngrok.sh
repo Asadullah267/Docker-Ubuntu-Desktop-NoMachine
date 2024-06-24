9cb55ef23553c48e54c2fd2b0b7b61a1452fd4de
Asadullah267 <work.venomvincint@gmail.com>
2024-06-24 20:32:38 +0500

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -8,6 +8,5 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
-tar -xvzf ngrok.tgz > /dev/null 2>&1
-
+wget -qO ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+tar -xzf ngrok.tgz > /dev/null 2>&1
